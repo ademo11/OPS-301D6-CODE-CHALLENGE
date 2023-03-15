ab8d663d7c78781c680e526466152cd373b0a9c3
Ademola Olatunbosun <ademomo11@gmail.com>
2023-03-15 03:33:13 +0000

    this is final

--- Code 1.sh
+++ Code 1.sh
@@ -12,5 +12,11 @@ cp /var/log/syslog $filename
 
 echo "Copied /var/log/syslog to $filename"
 
-Reference
+# Reference
 OpenAI
+
+# breakdown of the code The date command is used to get the current date and time in the format YYYY-MM-DD-HH-MM-SS.
+ # The filename is set with the timestamp by using the variable $timestamp.
+ # The cp command is used to copy the syslog file to the current working directory with the new filename.
+ # A message is printed to confirm that the file has been copied.
+s
