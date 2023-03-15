bd5900a6a5fcfc993b8ea8df67c20533f35f410f
Ademola Olatunbosun <ademomo11@gmail.com>
2023-03-15 03:26:35 +0000

    this is my message

---
+++ Code 1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Get current date and time in the format YYYY-MM-DD-HH-MM-SS
+timestamp=$(date +%Y-%m-%d-%H-%M-%S)
+
+# Set the filename with timestamp
+filename="syslog-$timestamp.log"
+
+# Copy the syslog file to the current working directory with the new filename
+cp /var/log/syslog $filename
+
+Refrences
+OpenAI
+
+echo "Copied /var/log/syslog to $filename"
