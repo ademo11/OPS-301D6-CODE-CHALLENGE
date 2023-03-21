9f4ad4898dd3d3b738752258600277808c77f9e8
Ademola Olatunbosun <ademomo11@gmail.com>
2023-03-21 02:10:38 +0000

    this is pyton

---
+++ code 5.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# How to use Linux/Bash commands within Python
+# First import the os library
+import os
+
+# Then use os.system to call any kind of bash command
+os.system("ls")
+
+os.system("ip a")
+
+os.system("lshw -short")
+
+# How to print to terminal
+
+print("Welcome to Python!")
+
