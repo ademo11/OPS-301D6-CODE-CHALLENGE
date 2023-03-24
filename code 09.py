07fbb616c63cfdce727778cc311454aee47c0ac0
Ademola Olatunbosun <ademomo11@gmail.com>
2023-03-24 01:45:44 +0000

    this is my code

---
+++ code 09.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# Script: OPS 301 Class 03 Ops Challenge Solution
+# Author: Ademola
+# Date of latest revision: 23 Mar 2023
+# Purpose:  print("b is greater than a") # you will get an error
+
+ademola = 42
+
+if ademola < 42 :
+ print("ademola is not the father.")
+
+elif ademola == 42:
+    print("ademola is the father.")
+
+# Greater than: a > b condinionals
+
+a = 33
+b = 33
+
+if a > b:
+ print("a is greater than b.")
+elif a <= b:
+ print("this is Ademola.")    
+
+# Use elif to add a condition that only checks if previous condition is not met
+age = 18
+
+if age < 18:
+    print("You are not old enough to vote.")
+elif age == 18:
+    print("You are old enough to vote for the first time!")
+else:
+    print("You are old enough to vote.")
+
+# Less than or equal to: a <= b statement 
+a = 33
+b = 200
+if  b >= a:
+    print("welcome home.")
+elif a >= b:
+    print("leave my house.")
+    
+# Not Equals: a != b conditional
+a = 12
+b = 12
+
+if a != b:
+    print("a is not equals b.")
+elif a == b:
+    print("a is equals to be.")    
+
